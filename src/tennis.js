d9916bede7e6b296782682dc62211a188c752068
Concha Asensio <concha@codium.team>
2024-02-21 22:45:26 +0100

    Refactor: extract getAdvantageScores method

--- src/tennis.js
+++ src/tennis.js
@@ -1,21 +1,10 @@
 'use strict';
 
-function isAdvantage(pointsPlayer1, pointsPlayer2) {
-  return (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) &&
-      (pointsPlayer1 - pointsPlayer2 === 1 || pointsPlayer1 - pointsPlayer2 ===
-          -1);
-}
-
 function getScore(pointsPlayer1, pointsPlayer2) {
   if (isTie(pointsPlayer1, pointsPlayer2)) {
     return getTieScores(pointsPlayer1);
   } else if (isAdvantage(pointsPlayer1, pointsPlayer2)) {
-    let minusResult = pointsPlayer1 - pointsPlayer2;
-    if (minusResult === 1) {
-      return 'Advantage player1';
-    } else if (minusResult === -1) {
-      return 'Advantage player2';
-    }
+    return getAdvantageScores(pointsPlayer1, pointsPlayer2);
   } else if (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) {
     let minusResult = pointsPlayer1 - pointsPlayer2;
     if (minusResult >= 2) {
@@ -34,6 +23,21 @@ function isTie(pointsPlayer1, pointsPlayer2) {
 function getTieScores(pointsPlayers) {
   let tieScores = ['Love-All', 'Fifteen-All','Thirty-All'];
   return tieScores[pointsPlayers] || 'Deuce';
+}
+
+function isAdvantage(pointsPlayer1, pointsPlayer2) {
+  return (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) &&
+      (pointsPlayer1 - pointsPlayer2 === 1 || pointsPlayer1 - pointsPlayer2 ===
+          -1);
+}
+
+function getAdvantageScores(pointsPlayer1, pointsPlayer2) {
+  let minusResult = pointsPlayer1 - pointsPlayer2;
+  if (minusResult === 1) {
+    return 'Advantage player1';
+  } else if (minusResult === -1) {
+    return 'Advantage player2';
+  }
 }
 
 function getRegularScores(pointsPlayer1, pointsPlayer2) {
