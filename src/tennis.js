3424489a93e660241659345aaa05ccb5a8806a58
Concha Asensio <concha@codium.team>
2024-02-21 22:33:58 +0100

    Refactor: simplify code removing switch

--- src/tennis.js
+++ src/tennis.js
@@ -15,16 +15,8 @@ function isTie(pointsPlayer1, pointsPlayer2) {
 }
 
 function getTieScores(pointsPlayer1) {
-  switch (pointsPlayer1) {
-    case 0:
-      return 'Love-All';
-    case 1:
-      return 'Fifteen-All';
-    case 2:
-      return 'Thirty-All';
-    default:
-      return 'Deuce';
-  }
+  let tieScores = ['Love-All', 'Fifteen-All','Thirty-All'];
+  return tieScores[pointsPlayer1] || 'Deuce';
 }
 
 function isAdvantageOrWin(pointsPlayer1, pointsPlayer2) {
